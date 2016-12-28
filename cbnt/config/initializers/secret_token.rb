b13a0142d7984f8a1654e5b0e9afee4199a8a36f
Héctor Palma Téllez <hector@palmatellez.cl>
2016-12-28 12:26:11 -0300

    Blank Rails app

---
+++ cbnt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cbnt::Application.config.secret_key_base = '29bdc49ba36fd5d7e9114866faa9b49d10bea36f731ef812766954d0a5b5dfc25a6ba3fae445c5665667f70e54b13a1489617ee944a9acc4cbadfdfe73a8932f'
