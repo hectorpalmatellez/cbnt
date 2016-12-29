588eb68e9ea27e14633f3c5389b8820e6a32f5ee
Héctor Palma Téllez <hector@palmatellez.cl>
2016-12-28 23:45:55 -0300

    Change root authenticated user stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Cbnt::Application.routes.draw do
   devise_for :users
-  get "welcome/index"
-  root "welcome#index"
+  get 'welcome/index'
+  root 'welcome#index'
 
   resources :docs
+  authenticated :user do
+    root 'docs#index', as: 'authenticated_root'
+  end
 end
